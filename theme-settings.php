8de2b9cf844cb14c2ae12a0962276179ff2ddd7f
Chris Herberte <chris@xweb.com.au>
2015-01-20 09:13:32 +1000

    theme settings functionality

---
+++ theme-settings.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Theme settings file for Aeon5.
+ *
+ */
+
+$form['max_width'] = array(
+  '#type' => 'select',
+  '#title' => t('Theme max width'),
+  '#default_value' => theme_get_setting('max_width'),
+  '#options' => array('780', '960'),
+  '#description' => t('Set the css max width (px) for the theme. 780px is the original Aeon5 width.'),
+);
+
