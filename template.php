f39c47798d4739d3106639c6bb850ae7058be85f
Chris Herberte <chris@xweb.com.au>
2015-01-20 07:24:12 +1000

    remove unused code

--- template.php
+++ template.php
@@ -1,14 +1,3 @@
 <?php
 
-/**
- * Implements hook_preprocess_layout().
- */
-function aeon5_preprocess_layout(&$variables) {
-  /*
-  if ($variables['content']['content']) {
-    $variables['content']['content'] .= '<pre>' . print_r($variables, TRUE);
-    $variables['content']['content'] = '<div class="clearfix">' . $variables['content']['content'] . '</div>';
-  }
-  */
-}
 
