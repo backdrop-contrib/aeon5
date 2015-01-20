be9390b6dcaa6d7c751fa549e771b0ccfd812055
Chris Herberte <chris@xweb.com.au>
2015-01-20 13:14:29 +1000

    add theme settings for site width

--- template.php
+++ template.php
@@ -1,7 +1,11 @@
 <?php
-
 /*
- * Initialise theme settings
+ * Implements hook_preprocess_page().
  */
+function aeon5_preprocess_page(&$variables) {
 
+  // initialise theme settings
+  $max_width = theme_get_setting('max_width');
+  drupal_add_css('@media all and (min-width: 851px) { .layout { max-width: ' . $max_width . 'px;}}', array('group' => CSS_THEME, 'type' => 'inline'));
+}
 
--- theme-settings.php
+++ theme-settings.php
@@ -10,7 +10,7 @@ $form['max_width'] = array(
   '#type' => 'select',
   '#title' => t('Theme max width'),
   '#default_value' => theme_get_setting('max_width'),
-  '#options' => array('780', '960'),
+  '#options' => array('780' => '780px', '960' => '960px'),
   '#description' => t('Set the css max width (px) for the theme. 780px is the original Aeon5 width.'),
 );
 
