cc52d357460f6b0bf0c359001bbeb8211517c0e7
Chris Herberte <chris@xweb.com.au>
2015-01-24 09:04:28 +1000

    color.module integration

--- template.php
+++ template.php
@@ -9,28 +9,8 @@
  * Implements hook_preprocess_page().
  */
 function aeon5_preprocess_page(&$variables) {
-  // initialise theme settings
-  require_once DRUPAL_ROOT . '/' . drupal_get_path('theme', 'aeon5') . '/includes/settings.inc';
-  $color_schemes = _aeon5_color_schemes();
-  $color_scheme = theme_get_setting('color_scheme');
-  $link = '#' . $color_schemes[$color_scheme]['link'];
-  $from = '#' . $color_schemes[$color_scheme]['from'];
-  $to = '#' . $color_schemes[$color_scheme]['to'];
-
-  $css = '';
-  $css .= 'a {color: ' . $link . ';}';
-  $css .= '.l-header, h2 {';
-  $css .= '  background: ' . $from . ';';
-  $css .= '  background: -moz-linear-gradient(top, ' . $from . ' 0%, ' . $to . ' 100%);';
-  $css .= '  background: -webkit-gradient(left top, left bottom, color-stop(0%, ' . $from . '), color-stop(100%, ' . $to . '));';
-  $css .= '  background: -webkit-linear-gradient(top, ' . $from . ' 0%, ' . $to . ' 100%);';
-  $css .= '  background: -ms-linear-gradient(top, ' . $from . ' 0%, ' . $to . ' 100%);';
-  $css .= '  background: linear-gradient(to bottom, ' . $from . ' 0%, ' . $to . ' 100%);';
-  $css .= '}';
-
   $max_width = theme_get_setting('max_width');
-  $css .= '@media all and (min-width: 851px) { .layout { max-width: ' . $max_width . 'px;}}';
-
+  $css = '@media all and (min-width: 851px) { .layout { max-width: ' . $max_width . 'px;}}';
   drupal_add_css($css, array('group' => CSS_THEME, 'type' => 'inline'));
 }
 
--- theme-settings.php
+++ theme-settings.php
@@ -2,26 +2,8 @@
 
 /**
  * @file
- * Theme settings file for Aeon5.
- *
  */
-
-require_once DRUPAL_ROOT . '/' . drupal_get_path('theme', 'aeon5') . '/includes/settings.inc';
-
-$form['max_width'] = array(
-  '#type' => 'select',
-  '#title' => t('Theme max width'),
-  '#default_value' => theme_get_setting('max_width'),
-  '#options' => backdrop_map_assoc(array('780', '960')),
-  '#description' => t('Set the css max width (px) for the theme. 780px is the original Aeon5 width.'),
+$form['color'] = array(
+  '#markup' => '<p>' . t('This theme supports custom color palettes if the Color module is enabled on the <a href="!url">modules page</a>. Enable the Color module to customize this theme.', array('!url' => url('admin/modules'))) . '</p>',
 );
-
-$form['color_scheme'] = array(
-  '#type' => 'select',
-  '#title' => t('Color scheme'),
-  '#default_value' => theme_get_setting('color_scheme'),
-  '#options' => backdrop_map_assoc(array_keys(_aeon5_color_schemes())),
-  '#description' => t('Set color scheme preset (does not require color module).'),
-);
-
 
